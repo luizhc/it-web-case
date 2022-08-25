000bca5c306c1528ee53ad3488a88341ea9fd646
Luiz Henrique <luizhenriquedeveloper@gmail.com>
2022-08-25 02:42:21 -0300

    feat: home

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
-  declarations: [NavbarComponent, FooterComponent],
+  declarations: [NavbarComponent, FooterComponent, HomeComponent],
   imports: [CommonModule],
-  exports: [NavbarComponent, FooterComponent],
+  exports: [NavbarComponent, FooterComponent, HomeComponent],
 })
 export class CoreModule {}
