6bf1823e8232e807f6ff0a3f05aaac225bc24ab8
Luiz Henrique <luizhenriquedeveloper@gmail.com>
2022-08-24 21:11:24 -0300

    feat: firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,20 @@
 import { NgModule } from '@angular/core';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore/';
 import { BrowserModule } from '@angular/platform-browser';
+import { getAnalytics } from 'firebase/analytics';
+import { initializeApp } from 'firebase/app';
 import { AppRoutingModule } from './app-routing.module';
+
+import { environment } from './../environments/environment';
 import { AppComponent } from './app.component';
+
+// Initialize Firebase
+const firebaseApp = initializeApp(environment.firebaseConfig);
+export const analytics = getAnalytics(firebaseApp);
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, AngularFirestoreModule],
   providers: [],
   bootstrap: [AppComponent],
 })
